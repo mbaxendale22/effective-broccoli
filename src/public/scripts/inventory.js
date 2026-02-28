01b86beaf22ac676b8198f21c9d2ae3f8d524e78
Matthew Baxendale <mdbaxendale1@gmail.com>
2026-02-28 17:39:12 +0000

    add stock take values

--- src/public/scripts/inventory.js
+++ src/public/scripts/inventory.js
@@ -1,6 +1,11 @@
 const openRoastModalButton = document.getElementById('open-roast-modal')
 const closeRoastModalButton = document.getElementById('close-roast-modal')
 const roastSessionModal = document.getElementById('roast-session-modal')
+const openStocktakeModalButton = document.getElementById('open-stocktake-modal')
+const closeStocktakeModalButton = document.getElementById(
+    'close-stocktake-modal'
+)
+const stocktakeModal = document.getElementById('stocktake-modal')
 
 if (openRoastModalButton && closeRoastModalButton && roastSessionModal) {
     openRoastModalButton.addEventListener('click', () => {
@@ -9,5 +14,15 @@ if (openRoastModalButton && closeRoastModalButton && roastSessionModal) {
 
     closeRoastModalButton.addEventListener('click', () => {
         roastSessionModal.close()
+    })
+}
+
+if (openStocktakeModalButton && closeStocktakeModalButton && stocktakeModal) {
+    openStocktakeModalButton.addEventListener('click', () => {
+        stocktakeModal.showModal()
+    })
+
+    closeStocktakeModalButton.addEventListener('click', () => {
+        stocktakeModal.close()
     })
 }
